fde472ce52c4094c09ea5e311dd8fc8afc44c1c0
lauracr03 <91657310+lauracr03@users.noreply.github.com>
2022-02-17 09:17:51 +0100

    Add files via upload

---
+++ proyecto2 junit/src/tiempo/CantidadNegativaException.java
@@ -0,0 +1,10 @@
+package tiempo;
+
+@SuppressWarnings("serial")
+public class CantidadNegativaException extends Exception {
+
+	public CantidadNegativaException(String string) {
+		super(string);
+	}
+
+}
